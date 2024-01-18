e732c767b50d6444a1b65f5cdfb008bd2bb77e93
seoldonghun <ehdgns5647@gmail.com>
2024-01-18 14:43:03 +0900

    두 수 비교하기

--- src/beginner/ex01.java
+++ src/beginner/ex01.java
@@ -1,7 +1,22 @@
 package beginner;
 
+import java.util.Scanner;
+
 public class ex01 {
     public static void main(String[] args) {
+        int A;
+        int B;
 
+        Scanner sc = new Scanner(System.in);
+        A = sc.nextInt();
+        B = sc.nextInt();
+
+        if(A > B){
+            System.out.println(">");
+        } else if (A < B) {
+            System.out.println("<");
+        }else if(A==B) {
+            System.out.println("==");
+        }
     }
 }
