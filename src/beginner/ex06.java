d5b06111896765a37e63c46a3a66cd59decfbb80
seoldonghun <ehdgns5647@gmail.com>
2024-01-19 09:12:14 +0900

    세수비교 테스트

--- src/beginner/ex06.java
+++ src/beginner/ex06.java
@@ -1,7 +1,41 @@
 package beginner;
 
+import java.io.*;
+
 public class ex06 {
     public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int []nums = new int[3];
+        int save;
+        try {
+            String num = br.readLine();
+            String[] result = num.split(" ");
 
+            for(String a:result){
+                nums = new int[]{Integer.parseInt(a)};
+            }
+
+            for (int i = 0; i < nums.length; i++) {
+                if(i == 0){
+                    continue;
+                }
+
+                if(nums[i-1] < nums[i]){
+                    save = nums[i-1];
+                    nums[i-1] = nums[i];
+                    nums[i] = save;
+                    i = 0;
+                }
+            }
+
+            for(int a:nums){
+                System.out.println(a);
+            }
+
+            bw.flush();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
